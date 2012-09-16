7543438517ca4165261402fc034228080017f4b1
Gui Lin <guileen@gmail.com>
2012-09-16 17:57:06 +0800

    update

--- examples/app.js
+++ examples/app.js
@@ -1,5 +1,15 @@
+var connect = require('connect');
 var pitaya = require('../lib/pitaya');
 var app = pitaya();
+
+app.tcp.use(function(req, res, next) {
+    console.log('TCP logger: ', req.url, req.message);
+    next();
+});
+
+app.http
+  .use(connect.cookieParser('tobo!'))
+  .use(connect.session());
 
 app.get('/', function(req, res) {
     res.data({
@@ -7,6 +17,34 @@ app.get('/', function(req, res) {
         msg: 'hello'
     })
 });
+
+app.get('/login', function(req, res) {
+    var userid = req.param('userid');
+    var password = req.param('password');
+    if(password == '1234') {
+      console.log('login as:' + userid);
+      req.session.userid = userid;
+      res.data({ret: 0, msg: 'login successfully'});
+    } else {
+      res.data({ret: -1, msg: 'wrong password'});
+    }
+});
+
+app.get('/friends', requireLogin, function(req, res) {
+    var userid = req.param('id');
+    res.data({
+        id: req.session.userid,
+        friends:[1,2,3,4,5,6,7,10, 1000, 2000, 12341123]
+    });
+})
+
+function requireLogin(req, res, next) {
+  if(!req.session.userid) {
+    res.data({ret: -1, msg: 'require login'});
+  } else {
+    next();
+  }
+}
 
 app.httpServer.listen(3000);
 app.tcpServer.listen(4000);
--- examples/tcp-client.js
+++ examples/tcp-client.js
@@ -1,3 +1,4 @@
+var msgpack = require('msgpack');
 var net = require('net');
 
 var HOST = '127.0.0.1';
@@ -6,29 +7,34 @@ var PORT = 4000;
 var client = new net.Socket();
 client.connect(PORT, HOST, function() {
 
-	console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-	// Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
-	var str = JSON.stringify({msg: 'hello'});
-	var len = Buffer.byteLength(str);
-	var buf = new Buffer(len + 2);
-	buf.writeUInt16BE(len, 0);
-	buf.write(str, 2);
-	client.write(buf);
-	// client.write('');
+    function send(msg) {
+      var buff = msgpack.pack(msg);
+      var len = buff.length;
+      var buf = new Buffer(len + 2);
+      buf.writeUInt16BE(len, 0);
+      buff.copy(buf, 2);
+      client.write(buf);
+    }
 
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
+    // client.write('');
+    send(['GET', '/', {msg: 'hello'}]);
+    send(['GET', '/friends']);
+    // send(['GET', '/login', {userid: '1234', password:'1234'}]);
+    // send(['GET', '/friends']);
 });
 
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
 client.on('data', function(data) {
 
-	console.log('DATA: ' + data);
-	// Close the client socket completely
-	client.destroy();
-
+    console.log('Length: ', data.length, 'Data:', data);
+    // Close the client socket completely
+    console.log(data.toString());
 });
 
 // Add a 'close' event handler for the client socket
 client.on('close', function() {
-	console.log('Connection closed');
+    console.log('Connection closed');
 });
